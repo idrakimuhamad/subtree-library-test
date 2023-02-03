f5577aedea7b7f1cf6fe0b0809d4e98aaac8a6d9
Idraki Muhamad <idrakimuhamad@gmail.com>
2023-02-03 17:00:08 +0800

    refactor: export source without extension

--- lib/ui/src/Button.tsx
+++ lib/ui/src/Button.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Pressable, Text } from 'react-native';
 
-export function Button({ title }) {
+export function Button({ title = '' }) {
   return (
     <Pressable>
       <Text>{title}</Text>
